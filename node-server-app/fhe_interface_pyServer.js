213fa1f4bec0793bc6b608bf1f2bd03cba23ad07
marcel <marcel@crowdbabble.com>
2018-07-27 04:41:54 -0400

    first commit for fhe interface in node using py server. need to create test cases. later add mult and add functions. beyond that should bind c++ functions in node

---
+++ node-server-app/fhe_interface_pyServer.js
@@ -0,0 +1,32 @@
+const request = require('sync-request');
+
+var fhePyAddr = "http://localhost:8082";
+
+var generateCryptoContext = function(){
+	var res request('POST', fhePyURL, {
+		body: "operation=cryptocontext"
+	});
+	return res.getBody();
+};
+
+var keygen = function(cryptocontext){
+	var res = request('POST', fhePyURL, { 
+		body: "operation=keygen&cryptocontext="+cryptocontext 
+	});
+	return res.getBody();
+};
+
+var encrypt = function(cryptocontext, publickey, plaintext){
+	var res = request('POST', fhePyURL, {
+		body: 'operation=encrypt&cryptocontext='+cryptocontext+'&publickey='+publickey+'&plaintext='+plaintext 
+	});
+	return res.getBody();
+};
+
+var decrypt = function(cryptocontext, privatekey, ciphertext){
+
+	var res = request('POST', fhePyURL, {
+		body: 'operation=decrypt&cryptocontext='+cryptocontext+'&privatekey='+privatekey+'&ciphertext='+ciphertext
+	});
+	return res.getBody();
+};
