4402e21c6b9b0902787951bfe6a6ac1d6ed6a58d
marcel <marcel@crowdbabble.com>
2018-07-27 20:37:49 -0400

    working interface to the fhe py server from node

--- fhe/shield_spark/fhe_server.py
+++ fhe/shield_spark/fhe_server.py
@@ -15,6 +15,8 @@ import socketserver
 import cgi
 import fhe
 import urllib
+import json
+
 class S(BaseHTTPRequestHandler):
 
 	def _bad_params(self):
@@ -40,59 +42,68 @@ class S(BaseHTTPRequestHandler):
 
 	def do_GET(self):
 		self._set_headers()
-		self.wfile.write( ("NO GET!").encode('latin-1'))
+		self.wfile.write( ("NO GET!").encode('utf-8'))
 
 	def do_HEAD(self):
 		self._set_headers()
 
 	def do_POST(self):
 		length = int(self.headers['Content-Length'])
-		post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('latin-1'))
-		# print(post_data)
-		
+		# post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
+		post_data = json.loads(self.rfile.read(length).decode('utf-8'))
+
+
 		# self._set_headers()
 		form = cgi.FieldStorage(
 			# fp=self.rfile,
 			# headers=self.headers,
 			# environ={'REQUEST_METHOD': 'POST'}
 		)
-		operation 	= post_data['operation'][0]
-		print("opp: " + operation[0])
+		operation 	= post_data['operation']
+
 		argOne 		= None
 		argTwo 		= None
 		argThree 	= None
 		if operation == 'cryptocontext':
 			print("HERERE 5555")
-			argOne 		= None
 		elif operation == 'keygen':
 			print("HERERE 66666")
-			argOne = post_data['cryptocontext'][0]
+			print("cc 1");
+			argOne = bytes(post_data['cryptocontext']['data']).decode('utf-8')
+			# print(post_data['cryptocontext']['data']);
 
 		elif operation == 'encrypt':
 			print("HERERE 7777")
-			argOne 		= post_data['cryptocontext'][0]
-			argTwo 		= post_data['publickey'][0]
-			argThree 	= post_data['plaintext'][0]
+			argOne 		= post_data['cryptocontext']
+			argTwo 		= post_data['publickey']
+			argThree 	= post_data['plaintext']
+
+			# argOne 		= post_data['cryptocontext'][0]
+			# argTwo 		= post_data['publickey'][0]
+			# argThree 	= post_data['plaintext'][0]
 
 		elif operation == 'decrypt':
 			print("HERERE 88888")
-			argOne 		= post_data['cryptocontext'][0]
-			argTwo 		= post_data['privatekey'][0]
-			argThree 	= post_data['ciphertext'][0]
+			argOne 		= post_data['cryptocontext']
+			print("HERERE 99999")
+			argTwo 		= post_data['privatekey']
+			print("HERERE 10101010")
+			argThree 	= bytes(post_data['ciphertext']['data']).decode('utf-8')
+			print("HERERE 12121212")
 		else:
 			self._bad_params()
-			self.wfile.write(('Bad operation supplied.').encode('latin-1'))
+			self.wfile.write(('Bad operation supplied.').encode('utf-8'))
 			return
 
 		ret = fhe.doOperationToStringSerialization(operation, argOne, argTwo, argThree)
 
 		if not isinstance(ret, str) or (isinstance(ret, int) and ret < 0):
 			self._internal_err()
-			self.wfile.write(('Invalid return.').encode('latin-1'))
+			self.wfile.write(('Invalid return.').encode('utf-8'))
 			return
 
 		self._set_headers_json()
-		self.wfile.write(ret.encode('latin-1'))
+		self.wfile.write(ret.encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=S, port=8082):
 	server_address = ('', port)
--- node-server-app/fhe_interface_pyServer.js
+++ node-server-app/fhe_interface_pyServer.js
@@ -1,28 +1,46 @@
 const request = require('sync-request');
 
-var fhePyAddr = "http://localhost:8082";
 
-/*
-	NEED TO PARSE JSON RETURN FOR PARTICULAR FIELDS
+var fs 		= require('fs');
 
-*/
+
+var fhePyURL = "http://localhost:8082";
+
 var generateCryptoContext = function(){
-	var res request('POST', fhePyURL, {
-		body: "operation=cryptocontext"
+	var res = request('POST', fhePyURL, {
+		json: {
+			operation: 'cryptocontext'
+		},
 	});
 	return res.getBody();
 };
 
+/*
+	returns :
+	{
+		cryptocontext: ...,
+		publickey: ...,
+		privatekey: ...
+	}
+*/
 var keygen = function(cryptocontext){
-	var res = request('POST', fhePyURL, { 
-		body: "operation=keygen&cryptocontext="+cryptocontext 
+	var res = request('POST', fhePyURL, {
+		json: {
+			operation: 'keygen',
+			cryptocontext: cryptocontext
+		},
 	});
-	return res.getBody();
+	return JSON.parse(res.getBody());
 };
 
 var encrypt = function(cryptocontext, publickey, plaintext){
 	var res = request('POST', fhePyURL, {
-		body: 'operation=encrypt&cryptocontext='+cryptocontext+'&publickey='+publickey+'&plaintext='+plaintext 
+		json: {
+			operation: 		'encrypt',
+			cryptocontext: 	cryptocontext,
+			publickey: 		publickey,
+			plaintext: 		plaintext
+		},
 	});
 	return res.getBody();
 };
@@ -30,10 +48,74 @@ var encrypt = function(cryptocontext, publickey, plaintext){
 var decrypt = function(cryptocontext, privatekey, ciphertext){
 
 	var res = request('POST', fhePyURL, {
-		body: 'operation=decrypt&cryptocontext='+cryptocontext+'&privatekey='+privatekey+'&ciphertext='+ciphertext
+		json: {
+			operation: 		'decrypt',
+			cryptocontext: 	cryptocontext,
+			privatekey: 	privatekey,
+			ciphertext: 	ciphertext
+		},
 	});
 	return res.getBody();
 };
 
 
-// most simple test is to 
+
+
+
+var assertTrue = function(val){
+	if(!val){
+		console.log("The error val was:");
+		console.log(val);
+		throw new Error('assert true error thrown')
+	}
+	return;
+}
+
+var testAll = function(){
+
+	var outfile = "./test_out.txt";
+	var plaintext = "a";
+
+	var cc = generateCryptoContext();
+
+	fs.writeFileSync(outfile, "cc is : \n\n");
+	fs.appendFileSync(outfile, cc);
+	fs.appendFileSync(outfile, "\n\n\n");
+
+	var keygenRet = keygen(cc);
+
+	var newCC = keygenRet.cryptocontext;
+	var privatekey = keygenRet.privatekey;
+	var publickey = keygenRet.publickey;
+
+	fs.appendFileSync(outfile, "newCC is : \n\n");
+	fs.appendFileSync(outfile, newCC);
+	fs.appendFileSync(outfile, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+	fs.appendFileSync(outfile, "privatekey is : \n\n");
+	fs.appendFileSync(outfile, privatekey);
+	fs.appendFileSync(outfile, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+	fs.appendFileSync(outfile, "publickey is : \n\n");
+	fs.appendFileSync(outfile, publickey);
+	fs.appendFileSync(outfile, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+
+	var encResult = encrypt(newCC, publickey, plaintext);
+
+	fs.appendFileSync(outfile, "encResult is : \n\n");
+	fs.appendFileSync(outfile, encResult);
+	fs.appendFileSync(outfile, "\n\n\n");
+
+	var decResult = decrypt(newCC, privatekey, encResult);
+
+	assertTrue(decResult == plaintext);
+}
+
+
+
+// most simple test is to perform a cc gen, keygen, encrypt, decrypt and check that it matches
+if(process.argv.indexOf("--test") > -1){
+	testAll();
+	console.log("FINISHED TESTS!");
+}
